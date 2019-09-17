90442831271c16500164637238813946973ad4ef
sunil.dhummi <sunil.dhummi@gmail.com>
2019-09-17 21:36:37 +0530

    added spotify oauth2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const passport = require('passport')
 const keys = require('./config/keys')
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const SpotifyStrategy = require('passport-spotify').Strategy;
+
 const app = express();
 
 //Ask passport to use google OAuth 2.0
@@ -44,6 +47,24 @@ passport.authenticate('facebook', {
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook'))
 
+
+//Spotify
+passport.use(new SpotifyStrategy({
+    clientID: keys.spotifyClientId,
+    clientSecret: keys.spotifyClientSecret,
+    callbackURL: '/auth/spotify/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('Access token: ', accessToken);
+    console.log('Refresh token: ', refreshToken);
+    console.log('Profile: ', profile);
+}));
+
+app.get('/auth/spotify',
+passport.authenticate('spotify', {
+    scope: ['user-read-email']
+}));
+
+app.get('/auth/spotify/callback', passport.authenticate('spotify'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
